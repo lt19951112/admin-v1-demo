b755b149ed1f3eae4355e69bf2c9b4dd22568b1e
林婷（恒羿诚） <479809887@qq.com>
2018-05-28 10:49:17 +0800

    优化修改

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import 'font-awesome/css/font-awesome.min.css'
-
+import Home from 'page/home/index.jsx'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router'
 import './test.scss'
+
+class App extends React.Component{
+  render () {
+    return (
+        <Router>
+            <Switch>
+              <Route exact path="/" Component="{Home}"/>
+              <Redirect from="*" to="/"/>
+            </Switch>
+        </Router>
+    );
+  }
+}
 ReactDOM.render(
-  <div>
-      <h1>Hello, world!</h1>
-  </div>,
+  <App/>,
   document.getElementById('app')
-);
+)
